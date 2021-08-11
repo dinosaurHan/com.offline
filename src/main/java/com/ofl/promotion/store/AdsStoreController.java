b1e6114ec1381825a0be699cc5ee4028042f1eb8
dinosaurHan <404567286@qq.com>
2021-08-12 01:08:52 +0800

    门店controller

---
+++ src/main/java/com/ofl/promotion/store/AdsStoreController.java
@@ -0,0 +1,16 @@
+package com.ofl.promotion.store;
+
+import com.ofl.promotion.common.entity.ResultDto;
+import com.ofl.promotion.guide.entity.OflGuide;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/store")
+public class AdsStoreController {
+
+    @RequestMapping(value = "list")
+    public ResultDto<OflGuide> selectAll(){
+        return new ResultDto<>();
+    }
+}
